46a81ca170e9c1b13ee44d6420e27ed9381f146a
Seif <seif.dorani@gmail.com>
2017-04-18 12:16:47 -0400

    update database

--- AlbumReview/db/schema.rb
+++ AlbumReview/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412154130) do
+ActiveRecord::Schema.define(version: 20170418161542) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20170412154130) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
