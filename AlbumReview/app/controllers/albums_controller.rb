b383233ea932a03e6de983f828e894ad475703c7
Seif <seif.dorani@gmail.com>
2017-04-18 13:55:25 -0400

    category renders accordingly

--- AlbumReview/app/controllers/albums_controller.rb
+++ AlbumReview/app/controllers/albums_controller.rb
@@ -1,7 +1,12 @@
 class AlbumsController < ApplicationController
   before_action :find_album, only: [:show, :edit, :update, :destroy]
   def index
-    @albums = Album.all.order("created_at DESC")
+    if params[:category].blank?
+      @albums = Album.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @albums = Album.where(:category_id => @category_id).order("created_at DESC")
+    end
   end
 
   def show
