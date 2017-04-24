bb996c4f13daae7daf1562d5976ff4fca536a3e5
Seif <seif.dorani@gmail.com>
2017-04-24 11:55:09 -0400

    associations, user_id to reviews, then updated db

--- AlbumReview/db/schema.rb
+++ AlbumReview/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424155232) do
+ActiveRecord::Schema.define(version: 20170424155434) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170424155232) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
