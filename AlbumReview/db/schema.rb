ba533ad6f68d5733f24d77c8750b7b4e56446c2c
Seif <seif.dorani@gmail.com>
2017-04-24 11:53:09 -0400

    new model review, attributes, then updated database, al associated files created

--- AlbumReview/db/schema.rb
+++ AlbumReview/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419151202) do
+ActiveRecord::Schema.define(version: 20170424155232) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170419151202) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
